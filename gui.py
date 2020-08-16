feeab77eb3d5a9461197f6883ad10762b6ec6c58
jungheonKim <sg00004@gmail.com>
2020-08-17 01:42:35 +0900

    DA_script_editor_gui
    Digimon Adventure PSP script editor for gui

---
+++ gui.py
@@ -0,0 +1,198 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow,QAction,QApplication,qApp,QFileDialog,QMessageBox
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+import scriptForGui as myfunc
+
+form_class = uic.loadUiType("gui.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.fileNumber=3520
+        self.statusBar().showMessage('Ready')
+        #ListWidget의 시그널
+        self.fileList.itemClicked.connect(self.clicked_script_name)
+        self.fileList.currentItemChanged.connect(self.clicked_script_name)
+        self.scriptsList.itemClicked.connect(self.clicked_script)
+        self.scriptsList.currentItemChanged.connect(self.clicked_script)
+        #버튼에 기능 연결
+        self.btn_open.clicked.connect(self.clicked_btn_open)
+        self.btn_insert.clicked.connect(self.insertText)
+        self.btn_save.clicked.connect(self.clicked_btn_save)
+
+
+        #액션 연결
+        self.actionopen.triggered.connect(self.clicked_btn_open)
+        self.actionSave.triggered.connect(self.clicked_btn_save)
+        self.actionSaveAs.triggered.connect(self.save_as)
+        self.actionOpen_ID3841_as.triggered.connect(self.clicked_btn_open_spsi)
+        self.actionSave_ID3841.triggered.connect(self.clicked_save_spsi)
+        self.actioninsert.triggered.connect(self.insertText)
+        self.actionNextText.triggered.connect(self.Next_text)
+        self.actionPrevText.triggered.connect(self.Prev_text)
+        self.actionexit.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+    #ListWidget의 시그널에 연결된 함수들
+    def clicked_save_spsi(self):
+        if self.switcgMode!='spsi':
+            self.statusBar().showMessage('This is not a ID03831')
+            return
+        myfunc.IDspsi_Import(self.inf,self.texts)
+        print(self.texts[0])
+        self.statusBar().showMessage('Save complete')
+        QMessageBox.information(self, 'Information', "Save complete", QMessageBox.Ok,QMessageBox.Ok)
+
+    def clicked_btn_save(self): 
+        if self.switcgMode!='script':
+            self.statusBar().showMessage('This is not a scriptfile')
+            return       
+        myfunc.script_import_gui(self.headerList,self.dialogNum,self.texts,self.inf)
+        self.statusBar().showMessage('Save complete')
+        QMessageBox.information(self, 'Information', "Save complete", QMessageBox.Ok,QMessageBox.Ok)
+
+    def save_as(self):        
+        if self.switcgMode!='script':
+            self.statusBar().showMessage('This is not a scriptfile')
+            return       
+        myfilter="Bin files (*.bin);;All files (*.*)"
+        savename=QFileDialog.getSaveFileName(self,'Save as...','./',myfilter,"Bin files (*.bin)")[0]
+        outf=open(savename,'rb+')
+        self.inf.seek(0)
+        outf.write(self.inf.read())
+        outf.seek(0)
+        myfunc.script_import_gui(self.headerList,self.dialogNum,self.texts,outf)
+        outf.close()
+        self.statusBar().showMessage('Save complete')
+        QMessageBox.information(self, 'Information', "Save complete", QMessageBox.Ok,QMessageBox.Ok)
+        
+    def insertText(self):
+        self.typingText=self.editText.toPlainText()
+        self.currentText.setPlainText(self.typingText)
+        currentRow=self.scriptsList.currentRow()
+        self.scriptsList.takeItem(currentRow)
+        if self.switcgMode=='spsi':
+            self.scriptsList.insertItem(currentRow,self.typingText)
+            self.texts[currentRow]=self.typingText
+            self.statusBar().showMessage('Insert complete')
+
+        else:
+            currentRow=str(currentRow)
+            for j in range(4):
+                if len(str(currentRow))<3:
+                    currentRow='0'+currentRow
+            self.scriptsList.insertItem(int(currentRow),currentRow+'. '+self.typingText)
+            iNum=sum(self.dialogNum[0:self.scriptName])+int(currentRow)
+
+            self.texts[iNum]=myfunc.str_to_bin(self.typingText,1)
+            self.statusBar().showMessage('Insert complete')
+
+    def clicked_script_name(self):
+        while True:
+            try:
+                self.scriptName=int(self.fileList.currentItem().text())-self.fileNumber
+                break
+            except:
+                break
+        num=0
+        self.currentText.clear()
+        self.editText.clear()
+        self.scriptsList.clear()
+        self.speakerName.clear()
+        self.listName=[]
+        if self.switcgMode=='spsi':
+            for i in range(len(self.texts)):
+                self.scriptsList.addItem(self.texts[i])#myfunc.str_to_bin(self.texts[i],2))
+        else:
+            for i in range(self.dialogNum[self.scriptName]):
+                num=str(num)
+                for j in range(4):
+                    if len(str(num))<3:
+                        num='0'+num
+                self.iNum=i+sum(self.dialogNum[0:self.scriptName])
+                self.scriptsList.addItem(num+'. '+myfunc.str_to_bin(self.texts[self.iNum],2))
+                self.listName.append(self.speakerAndDialogs[0][self.iNum])
+
+                num=int(num)+1
+            self.scriptsList.setCurrentRow(0)
+            self.statusBar().showMessage('Script number %d is loaded'%(self.scriptName+self.fileNumber))
+    
+    def clicked_script(self):
+        if self.switcgMode=='spsi':
+            while True:
+                try:
+                    self.itemscript=self.scriptsList.currentItem().text()
+                    break
+                except :
+                    break
+                    
+        else:
+            while True:
+                try:
+                    self.itemscript=self.scriptsList.currentItem().text()[5:]
+                    self.itemNumber=int(self.scriptsList.currentItem().text()[0:3])
+                    #print(self.scriptName) #스크립트 번호반환
+                    #print(self.scriptsList.currentItem().text()[0:3]) # 현재 나열된 텍스트의 제일앞 번호 3자리
+                    break
+                except :
+                    break
+            #for i in self.texts:
+                #print(myfunc.str_to_bin(i,2))
+            num=0
+            #self.text.clear()
+            currentName=myfunc.speakerNameIntToStr(self.listName[self.itemNumber])
+            self.speakerName.setPlainText(currentName)
+            #print(self.listName[self.itemNumber])
+        self.currentText.setPlainText(self.itemscript)
+        self.editText.setPlainText(self.itemscript)
+        #print(self.currentText.toPlainText())
+
+    #버튼 함수
+    def clicked_btn_open(self):
+        self.switcgMode='script'
+        self.fileList.clear()
+        self.headerList=[]
+        self.dialogNum=[]
+        self.texts=[]
+
+        #self.filename = QFileDialog.getOpenFileName(self)[0]
+        self.filename='onlytext_test1.bin'
+        self.inf = open(self.filename,'rb+')
+        self.data=self.inf.read()
+
+        self.headerList=myfunc.find_header(b'\x45\x54\x44\x46',self.data)
+        self.dialogNum=myfunc.dialog_num(self.headerList,self.data)
+
+        for i in range(len(self.dialogNum)):
+            self.fileList.addItem(str(i+self.fileNumber))
+        self.speakerAndDialogs=myfunc.script_extract(self.headerList,self.dialogNum,self.inf) 
+        self.texts=self.speakerAndDialogs[1]
+        self.fileList.setCurrentRow(0)
+    def clicked_btn_open_spsi(self):
+        self.switcgMode='spsi'
+        self.fileList.clear()
+        self.texts=[]
+        self.filename = QFileDialog.getOpenFileName(self)[0]
+        #self.filename = 'ID03841'
+        self.fileList.addItem(self.filename)
+        self.inf=open(self.filename,'rb+')
+        self.texts=myfunc.IDspsi_Extract(self.inf)
+        #self.scriptslist.addItem()
+        self.fileList.setCurrentRow(0)
+
+    def Next_text(self):
+        self.scriptsList.setCurrentRow(self.scriptsList.currentRow()+1)
+    def Prev_text(self):
+        self.scriptsList.setCurrentRow(self.scriptsList.currentRow()-1)
+
+if __name__ == "__main__" :
+    print("Start gui for dgmAd psp scripter")
+    
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
---
+++ scriptForGui.py
@@ -0,0 +1,309 @@
+import os
+import sys
+import pandas as pd
+import re
+def main():
+    print(str_to_bin("Test str2bin mode 1 : 아아아아뷁",1))
+    
+    
+def test():
+        switcgMode='script'
+        headerList=[]
+        dialogNum=[]
+        texts=[]
+
+        #filename = QFileDialog.getOpenFileName(self)[0]
+        filename='onlytext_test1.bin'
+        inf = open(filename,'rb+')
+        data=inf.read()
+
+        headerList=find_header(b'\x45\x54\x44\x46',data)
+        dialogNum=dialog_num(headerList,data)
+
+
+        speakerAndDialogs=script_extract(headerList,dialogNum,inf) 
+
+        outfp=open('names.txt','w+')
+        for i in range(len(speakerAndDialogs[0])):
+            outfp.write(speakerAndDialogs[0][i]+'\n')
+
+def speakerNameIntToStr(Int):
+    Int=str(Int)
+    pathCsv = '오프셋별화자이름.xlsx'
+    pd.read_excel(pathCsv)
+    for 
+    return Int
+def IDspsi_GetlistOffset_ex(fp):
+    numOfScripts=278
+    OffsetStartOffset = 16
+    fp.seek(OffsetStartOffset,0)
+    listOffset = []
+    for i in range(numOfScripts):
+        Offset=int.from_bytes(bytes=fp.read(2),byteorder='little')
+        listOffset.append(Offset)
+        fp.seek(2,1)
+    return listOffset
+def IDspsi_GetlistScripts_ex(fp,listOffset):
+    numOfScripts=278
+    OffsetStartScript=1128
+    listScripts=[]
+    fp.seek(OffsetStartScript,0)
+    OffsetEndOfScript=0
+
+    for i in range(numOfScripts):
+        fp.seek(listOffset[i])    #대사 시작점으로 이동
+        while True:
+            try:
+                if numOfScripts-1==i:              #else에서 행하는 i+1이 루프의 마지막 차례에서 i범위를 초과함으로 예외처리
+                    finishData=fp.read(100)
+                    length=finishData.find(b'\x00')
+                    text=finishData[0:length]
+                    text=str_to_bin(text,2)
+                else:
+                    length=listOffset[i+1]-listOffset[i]-1
+                    text=fp.read(length)
+                    length2=text.find(b'\x00')
+                    if length2!=-1:length=length2
+                    else : length=length
+                    text=text[0:length]
+                    text=str_to_bin(text,2)
+                listScripts.append(text)
+                break
+            except IndexError:break
+    return listScripts
+
+def IDspsi_Extract(fp):
+    listOffset=IDspsi_GetlistOffset_ex(fp)
+    return IDspsi_GetlistScripts_ex(fp,listOffset)
+
+def IDspsi_SetOffset_im(fp,listScripts):
+    numOfScripts=278
+    OffsetStartOffset = 16
+    OffsetStartScript=1128
+    fp.seek(OffsetStartOffset,0)
+
+    for i in range(numOfScripts):
+        if i!=0:
+            writeOffset=lengths
+            fp.write(int.to_bytes(writeOffset,2,'little'))
+        else : 
+            firstOffset=int.from_bytes(bytes=fp.read(2),byteorder='little')
+            fp.seek(-2,1)
+            fp.write(int.to_bytes(firstOffset,2,'little'))
+            lengths=firstOffset
+        while True:
+            try:
+                text = listScripts[i]
+                text = str_to_bin(text,1)
+                break
+            except LookupError or IndexError: 
+                print('i = %d, Offset= %s, text= %s'%(i,fp.tell(),texts[i-1]))
+                exit()
+        length = len(text)
+        lengths+=length+1
+        fp.seek(2,1)
+    for i in range(numOfScripts):
+        text=str_to_bin(listScripts[i],1)
+        fp.write(text)
+        fp.write(b'\x00')
+    finishOffset=fp.tell()
+    endOfFp=fp.seek(0,2)
+    fp.seek(finishOffset,0)
+    while fp.tell()!=endOfFp:
+        fp.write(b'\x00')
+
+def IDspsi_Import(fp,listScripts):
+
+    IDspsi_SetOffset_im(fp,listScripts)
+
+
+
+
+def find_dialog(textf):
+    retlist=[]
+    lines=textf.readlines()
+    for line in lines:
+        which=line.find(",")
+        which2=line[which+1:].find(",")
+        line=line[which+which2+2:].replace("\n","")
+        retlist.append(line)
+    return retlist
+
+def dialog_num(headerList,data):
+    dialogOffset=[]
+    for i in headerList:
+        dialogOffset.append(int.from_bytes(data[i+12:i+14],byteorder='little'))
+    return dialogOffset
+
+def find_header(find_str,data):
+    count=0
+    findOffset=[]
+    where=data.find(find_str)
+    findOffset.append(where)
+    count+=1
+    while True:
+        where=data[findOffset[count-1]+4:].find(find_str)
+        if where==-1 :
+            break
+        findOffset.append(where+4+findOffset[count-1])
+        count+=1
+    return findOffset
+
+def str_to_bin(string_,sw):
+    pathTbl='tbl.txt'
+    tbl=open(pathTbl,'r',encoding='utf-16')
+    kor=tbl.readline()
+    jpn=tbl.readline()
+    while True:
+        try:
+            if sw==1:                                               # Str to Bin : 입력기에서 사용
+                string_=string_.replace('\n','&')
+                string_=string_.translate(str.maketrans(
+                    kor+'&①②③',
+                    jpn+'＆１２３'))
+                string_=bytes(string_,'shift-jis',errors='replace')#'ignore')
+                string_=string_.replace(b'\x81\x95',b'\xff\x80')
+                return string_
+            elif sw==2:                                             # Bin to Str : 출력기에서 사용
+                string_=string_.replace(b'\xFF\x80',b'\x81\x95')    # 개행을 전각&으로 출력
+                string_=string_.replace(b'\xFF\x44',b'\x81\x83')    # 이하 < or >
+                string_=string_.replace(b'\xFF\x42',b'\x81\x83')
+                string_=string_.replace(b'\xFF\x40',b'\x81\x84')
+                string_=string_.replace(b'\xFF\x00',b'\x81\x84')
+                string_=str(string_,encoding='shift-jis',errors='replace')#'ignore')
+                string_=string_.translate(str.maketrans(jpn,kor))
+                string_=string_.replace('＆','\n')
+                string_=string_.replace('&','\n')
+                string_=string_.replace('+','…')
+
+                return string_
+        except UnicodeDecodeError or UnicodeEncodeError or LookupError as err:
+            print("error: {0}".format(err))
+            print('test')
+            outerrlog=open('errLog.log','w')
+            print(string_)
+            outerrlog.write(string_)
+            outerrlog.close()
+            exit()
+
+# 스크립트 추출기 8월5일 수정사항
+#   이중배열로 추출함 dialog[0]에는 화자가 누군지, [1]에는 대사가 들어감
+def script_extract(headerList,dialogNum,inf):
+    count=0
+    texts = ''
+    dialogs=[[],[]]
+    for h,d in zip(headerList,dialogNum):
+        inf.seek(h)
+        inf.seek(8,1)
+        strangenum=inf.read(2)
+        strangenum=int.from_bytes(bytes=strangenum,byteorder='little')   
+        inf.seek(strangenum*16+6,1)
+        lengths=0     
+        inf.seek(16,1)
+        
+        dialogOffsets=[]
+        speaker=[]
+        for i in range(d):                  #대사 오프셋 리스트 작성
+            name=speakerNameIntToStr(int.from_bytes(bytes=inf.read(2),byteorder='little'))
+            dialogs[0].append(name)
+            inf.seek(18,1)
+            dialogOffsets.append(int.from_bytes(bytes=inf.read(2),byteorder='little'))
+            inf.seek(10,1) 
+
+        for i in range(d):                  #대사 리스트 작성. 대사 갯수만큼 반복
+            inf.seek(h+dialogOffsets[i])    #대사 시작점으로 이동
+            while True:
+                try:
+                    if d-1==i:              #else에서 행하는 i+1이 마지막에 d리스트의 범위를 초과함으로 예외처리
+                        finishData=inf.read(100)
+                        length=finishData.find(b'\x00')
+                        text=finishData[0:length]
+                    else:
+                        length=dialogOffsets[i+1]-dialogOffsets[i]
+                        text=inf.read(length)
+                        length=text.find(b'\x00')
+                        #if not length == -1:
+                            #text=text[0:length]
+                    dialogs[1].append(text)
+                    break
+                except IndexError:
+                    break
+    print(len(dialogs[1][1]))
+    print(dialogs[1][1])
+    return dialogs
+
+def script_import_gui(headerList,dialogNum,texts,inf):
+    count=0
+    finishOffset=0
+    for h,d in zip(headerList,dialogNum):
+        if inf.tell()==0:
+            pass
+        else:
+            ETDFOffset=h
+            num=ETDFOffset-finishOffset
+            data=inf.read(num)
+            pBinOffset=data.find(b'\x70\x42\x69\x6E')
+            inf.seek(finishOffset)
+            hex00appender(pBinOffset,inf)
+        #Currentdata=data[h:]
+        inf.seek(h)
+        #print('Go to ETDF Offset')
+        #print(inf.tell())
+        inf.seek(8,1)
+        strangenum=inf.read(2)
+        strangenum=int.from_bytes(bytes=strangenum,byteorder='little')
+
+        #Currentdata=Currentdata[strangenum*16+8:]
+        inf.seek(strangenum*16+6,1)
+        lengths=0
+        #print('Go to dialogs length Offset')
+        #print(inf.tell())
+        inf.seek(36,1)
+        for i in range(d):
+            if i == 0: 
+                firstOffset=int.from_bytes(bytes=inf.read(2),byteorder='little')
+                #print(hex(firstOffset))
+            else: 
+                writeOffset=lengths
+                inf.write(int.to_bytes(writeOffset,2,'little'))
+
+            while True:
+                try:
+                    text=texts[count+i]
+                    #text=str_to_bin(text,1)
+                    break
+                except LookupError or IndexError:
+                    print('i = %d, d= %d, Offset= %s, text= %s'%(i,d,inf.tell(),texts[count+i-1]))
+                    exit()
+            length=len(text)
+            if i==0:lengths=firstOffset+length+1
+            else:lengths+=length+1
+            inf.seek(30,1)
+        
+        inf.seek(-20,1)
+        #print('Go to dialogs Offset')
+        #print(inf.tell())
+        for i in range(d):
+            text=texts[count+i]
+            while True:
+                try:
+                    #text=str_to_bin(text,1)
+                    break
+                except LookupError:
+                    print(inf.tell())
+                    print(text)
+                    exit()
+            inf.write(text)
+            #print(inf.read(10))
+            inf.write(b'\x00')
+        finishOffset=inf.tell()
+        count+=d
+def hex00appender(num,outf):
+    ff=b'\x00' # 추가 할 값
+    i=0
+    while i<num :
+        outf.write(ff)
+        i+=1
+
+if __name__ == "__main__":
+    main()
